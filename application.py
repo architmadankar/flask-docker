f4b54ef5472ca2393a81a84406da6d6e265e4d0c
Anshoool <architmadankar21@gmail.com>
2023-09-17 02:25:50 +0530

    attached with template

--- application.py
+++ application.py
@@ -2,12 +2,12 @@
 # from: https://flask.palletsprojects.com/en/1.1.x/quickstart/
 
 # python dependencies are declared in the requirements.txt file
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
